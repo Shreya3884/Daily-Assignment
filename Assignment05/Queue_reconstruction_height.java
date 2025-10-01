9078767340d83a18ccbf916276ee47327eb54945
Shreya3884 <sj0711778@gmail.com>
2025-10-01 23:22:26 +0530

    ass

--- Assignment05/Queue_reconstruction_height.java
+++ Assignment05/Queue_reconstruction_height.java
@@ -1,4 +1,4 @@
-lass Solution {
+class Solution {
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
         List<int[]> res = new LinkedList<>();
