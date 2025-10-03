46bf8e4c2badc6909493c95d738cd6509bb59ea4
Shreya3884 <sj0711778@gmail.com>
2025-10-03 23:33:42 +0530

    ass

---
+++ Assignment06/combination.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combination{
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(1, n, k, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int start, int n, int k, List<Integer> curr, List<List<Integer>> res) {
+        if (curr.size() == k) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = start; i <= n - (k - curr.size()) + 1; i++) {
+            curr.add(i);
+            backtrack(i + 1, n, k, curr, res);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
+ 
